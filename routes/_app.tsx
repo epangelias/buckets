9eee5342b09d94a077b904c9a81a4325a143f82d
Epangelias <epangealis@gmail.com>
2024-05-04 23:29:17 -0700

    webapp fix

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,7 +4,6 @@ export default function App({ Component }: PageProps) {
 		<html>
 			<head>
 				<meta charset='utf-8' />
-				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<meta name='color-scheme' content='light dark' />
 				<title>Hash Buckets</title>
 				<link rel='stylesheet' href='/styles.css' />
@@ -13,6 +12,13 @@ export default function App({ Component }: PageProps) {
 					href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪣</text></svg>`}
 				>
 				</link>
+				<meta name='apple-mobile-web-app-capable' content='yes' />
+				<meta name='msapplication-tap-highlight' content='no' />
+				<link
+					rel='apple-touch-icon'
+					href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪣</text></svg>'
+				/>
+				<meta content='minimum-scale=1.0, width=device-width, maximum-scale=1, user-scalable=no' name='viewport' />
 			</head>
 			<body>
 				<Component />
