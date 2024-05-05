d13681d5f5e43d7c4b6c093d8d2d4d2e4adffaf5
Epangelias <epangealis@gmail.com>
2024-05-04 22:53:40 -0700

    added raw option

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
 import BucketEdit from '../islands/BucketEdit.tsx';
 import { FreshContext } from '$fresh/server.ts';
+import { decryptText } from '../helpers/crypto.ts';
 
 const db = await Deno.openKv();
 
@@ -8,10 +9,16 @@ export default async function Home(req: Request, ctx: FreshContext) {
 
 	const hash = ctx.url.searchParams.get('hash');
 	const key = ctx.url.searchParams.get('key');
+	const raw = !!ctx.url.searchParams.get('raw');
 
 	if (hash) {
 		const res = await db.get(['blob', hash]);
 		if (res.versionstamp !== null) text = res.value as string;
+	}
+
+	if (raw) {
+		if (key) return new Response(await decryptText(text, key));
+		else return new Response(text);
 	}
 
 	return (
